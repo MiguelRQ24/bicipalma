bc395e623053144b99d815eaa159f5a4991c7aef
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-02-17 15:09:05 +0100

    feat(Anclajes.java): Crear clase Anclajes.java

---
+++ src/main/java/edu/estatuas/domain/estacion/Anclajes.java
@@ -0,0 +1,14 @@
+package edu.estatuas.domain.estacion;
+
+public class Anclajes {
+    private Anclaje[] anclajes;
+    Anclajes(int cantidad_anclajes){
+        this.anclajes = new Anclaje[cantidad_anclajes];
+        crearAnclajes(cantidad_anclajes);
+    }
+    private void crearAnclajes(int cantidad_anclajes){
+        for (int i = 0; i < cantidad_anclajes;i++){
+            this.anclajes[i] = new Anclaje();
+        }
+    }
+}
