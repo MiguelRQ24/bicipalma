98d74082e382ba175843bfb1ea8d029a7ded50d7
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2025-02-17 14:45:31 +0100

    Crear structura del proyecto.

---
+++ src/main/java/edu/estatuas/appalma/BiciPalma.java
@@ -0,0 +1,13 @@
+package edu.estatuas.appalma;
+
+/**
+ * Hello world!
+ *
+ */
+public class BiciPalma
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
